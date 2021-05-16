bfb902dda57956daa7b0b6a793bc36321e3cbbb9
sunilpatil9194 <sunil.patil@nxglabs.com>
2021-05-16 10:43:11 +0530

    array includes method

---
+++ 10_includes/includes1.js
@@ -0,0 +1,8 @@
+// includes - used to find out if value is inside an an array
+
+const names = ["Devid", "Maxwel", "Sam", "Smith"];
+
+const result = names.includes("Sam");
+
+console.log(result); // true
+
