c1dd68e4d2c02dd233bda1d0046fcb4e91275b84
sunilpatil9194 <sunil.patil@nxglabs.com>
2021-05-16 11:35:40 +0530

    array shift method

---
+++ 16_shift/shift.js
@@ -0,0 +1,9 @@
+// shift - used to remove the first element from an array.
+// this method will also return the removed element
+
+const numbers = [1, 2, 3, 4, 5];
+
+let removed = numbers.shift();
+
+console.log(removed); // 1
+console.log(numbers); // [2, 3, 4, 5]
