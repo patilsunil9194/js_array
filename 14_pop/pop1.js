e5372902ebd3f95ebe5ad16a010bacb0abd0944a
sunilpatil9194 <sunil.patil@nxglabs.com>
2021-05-16 11:18:17 +0530

    array pop method

---
+++ 14_pop/pop1.js
@@ -0,0 +1,8 @@
+// pop - used to remove the last element of an array
+
+const numbers = [1, 2, 3, 4, 5];
+
+const lastItem = numbers.pop();
+
+console.log(numbers); //[ 1, 2, 3, 4 ]
+console.log(lastItem); // 5
