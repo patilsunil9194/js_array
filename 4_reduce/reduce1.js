99f5884c975bc756c1d7a1f784a1eca2479254a2
Sunil Patil <59803509+patilsunil9194@users.noreply.github.com>
2021-05-22 11:07:42 +0530

    Update reduce1.js

--- 4_reduce/reduce1.js
+++ 4_reduce/reduce1.js
@@ -1,3 +1,7 @@
+// reduce - used to execute a callback function (or a reducer function) on each element of the array resulting in a single output value.
+// The reducer function's returned value is assigned to the accumulator, 
+// which can be used across each iteration and ultimately becomes the final, single resulting value.
+
 const numbers = [1, 2, 3, 4, 5];
 
 // reduce(callback,initialValue)
