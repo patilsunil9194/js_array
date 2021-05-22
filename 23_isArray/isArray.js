125dace4f4ea3e5cda60b8385e3e5e459455a5c0
sunilpatil9194 <sunil.patil@nxglabs.com>
2021-05-22 10:14:25 +0530

    array isArray methods

---
+++ 23_isArray/isArray.js
@@ -0,0 +1,9 @@
+// isArray - used to check if a given value is an array
+
+const arr = [1, 2, 3, 4, 5];
+const str = "12345666";
+const num = 34;
+
+console.log(Array.isArray(arr)); // true
+console.log(Array.isArray(str)); // false
+console.log(Array.isArray(num)); // false
